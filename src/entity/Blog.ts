1f043d63e5be4907c19143e14de0cf0488b0bcc5
nguyenquynh46 <nguyenthiquynhhvnh93@gmail.com>
2023-07-26 15:08:16 +0700

    tao blog

---
+++ src/entity/Blog.ts
@@ -0,0 +1,17 @@
+import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Blog {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: "varchar", length: 255})
+    name: string;
+    @Column({type: "varchar", length: 255})
+    title: string;
+
+    @Column({type: "varchar", length: 255})
+    des: string;
+
+
+}
---
+++ src/service/BlogService.ts
@@ -0,0 +1,20 @@
+import {Service} from "./Service";
+import {Blog} from "../entity/Blog";
+
+class BlogService implements Service<Blog>{
+    add(data) {
+    }
+
+    delete(id) {
+    }
+
+    findAll() {
+    }
+
+    findById(id) {
+    }
+
+    update(id, data) {
+    }
+
+}
