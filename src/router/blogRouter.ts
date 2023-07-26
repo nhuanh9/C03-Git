9b067e208029556b368078eb6acc21cd38a96e2e
Tru Nguyen <nguyentruhn@gmail.com>
2023-07-26 16:39:06 +0700

    tru_hieu

--- src/router/blogRouter.ts
+++ src/router/blogRouter.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import blogController from "../controller/blogController";
-import {auth} from "../middleware/jwt";
+
 
 const blogRouter= Router();
 blogRouter.get('/:id',blogController.findById)
